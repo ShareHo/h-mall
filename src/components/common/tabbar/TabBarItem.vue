b8da19c8d9aa2b0c826abdb0d1853d6a53ab40b6
Exit-hzx <734186178@qq.com>
2020-04-22 11:04:40 +0800

    页面tabbar及路由初始化

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <!--所有的item都展示同一个图片, 同一个文字-->
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: "red"
+    }
+  },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.includes(this.path);
+    },
+    activeStyle() {
+      return this.isActive ? { color: this.activeColor } : {};
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path).catch(err => {});
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 12px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 6px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
